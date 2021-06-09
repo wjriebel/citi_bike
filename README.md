a98805b3fd343190f94f5f90e275ff523822ec05
Joe Riebel <joeyriebel0@gmail.com>
2021-06-09 13:40:16 -0500

    update 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # citi_bike
+
+This is a project to map out the rental bike stations in NYC
---
+++ assets/js/config.js
@@ -0,0 +1,1 @@
+const API_KEY = 'pk.eyJ1Ijoid2pyaWViZWwiLCJhIjoiY2tuNnV4MzM1MGhrbjJucWZnNDhtd2ZyaiJ9.2KY5_BQ_DwKd1xMYYXG38w'
